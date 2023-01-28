34411da94e6e799ceba4c4f44c49af41d83fa4c3
DeonGoh <deongoh.255@gmail.com>
2023-01-28 19:16:04 +0800

    working sign-up & sign-in functions

--- login-script.js
+++ login-script.js
@@ -3,6 +3,9 @@ $(document).ready(function () {
     const APIKEY = "63ce80d5969f06502871b127";
 
     function loginCheck(){
+        var usernameInput = $('#username-input').val();
+        var passwordInput = $('#password-input').val();
+
         var settings = {
             "async": true,
             "crossDomain": true,
@@ -18,12 +21,8 @@ $(document).ready(function () {
     
         $.ajax(settings).done(function (response) {
             console.log(response); 
-            
-            for (var i = 0; i < response.length; i++) {
-                
-                var usernameInput = $('#username-input').val();
-                var passwordInput = $('#password-input').val();
 
+            for (var i  = 0; i < response.length; i++) {
                 if(response[i].Username == usernameInput && response[i].Password == passwordInput){
                     sessionStorage.setItem("username", response[i].Username);
                     console.log("LOGIN SUCCESSFULLY");
@@ -36,27 +35,4 @@ $(document).ready(function () {
     $("#login-submit").on("click", function(e){
         loginCheck();
     });
-
-    function postDetails(){
-        var jsondata = {"field1": "xyz","field2": "abc"};
-        var settings = {
-        "async": true,
-        "crossDomain": true,
-        "url": "https://idasg2-e35e.restdb.io/rest/account",
-        "method": "POST",
-        "headers": {
-            "content-type": "application/json",
-            "x-apikey": APIKEY,
-            "cache-control": "no-cache"
-        },
-        "processData": false,
-        "data": JSON.stringify(jsondata)
-        }
-
-        $.ajax(settings).done(function (response) {
-            console.log(response);
-        });
-    }
-
-    
 });
---
+++ signup-script.js
@@ -0,0 +1,76 @@
+$(document).ready(function(){
+    const APIKEY = "63ce80d5969f06502871b127";
+
+    function signUp(){
+        var username = $('#username-input').val();
+        var password = $('#password-input').val();
+
+        var jsondata = {
+            "Username" : username,
+            "Password" : password
+        }
+
+        var settings = {
+            "async": true,
+            "crossDomain": true,
+            "url": "https://idasg2-e35e.restdb.io/rest/account",
+            "method": "POST",
+            "headers": {
+                "content-type": "application/json",
+                "x-apikey": APIKEY,
+                "cache-control": "no-cache"
+            },
+            "processData": false,
+            "data": JSON.stringify(jsondata)
+        }
+
+        $.ajax(settings).done(function (response) {
+            console.log(response);
+        });
+    }
+
+    
+    $("#signUp").on('click',function (event){
+        event.preventDefault();
+    
+        var usernameInput = $('#username-input').val();
+
+        var settings = {
+            "async": true,
+            "crossDomain": true,
+            "url": "https://idasg2-e35e.restdb.io/rest/account",
+            "method": "GET",
+            "headers": {
+            "content-type": "application/json",
+            "x-apikey": APIKEY,
+            "cache-control": "no-cache"
+            }
+        }
+       
+    
+        $.ajax(settings).done(function (response) {
+            console.log(response); 
+
+            for (var i  = 0; i < response.length; i++) {
+                if(response[i].Username == usernameInput ){
+                   console.log("Invalid Username as its not unique");
+                   alert("Username is already registered. Please try another");
+                }
+            }
+
+            let password = $("#password-input").val();
+            let check_password = $("#check-password").val();
+
+            if (password == ""){
+                alert("Field cannot be empty.");
+            }
+            else if(password != check_password){
+                alert("Password didn't match try again.");
+            }
+            else if(password == check_password){
+                signUp();
+            }
+            
+        });   
+    })
+})
